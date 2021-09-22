b1c34c5c6a6e61efe2fb5ac6bdf8ff7826ee6c77
HitJatin <adriano23012001@gmail.com>
2021-09-22 21:24:28 +0530

    new

--- Pattern_Half_Pyramid_Numbers.java
+++ Pattern_Half_Pyramid_Numbers.java
@@ -1,16 +1,17 @@
 public class Pattern_Half_Pyramid_Numbers {
     /**
      * Print the Pattern 
-     *  *
-     *  **
-     *  ***
-     *  ****
+     *  1
+     *  12
+     *  123
+     *  1234
+     *  12345
      */
     public static void main(String[] args) {
-        int m=4;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+        int m=5;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
