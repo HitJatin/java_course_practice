be32d6736ec550b9f004b3d732ccfebc7d72e101
HitJatin <adriano23012001@gmail.com>
2021-09-25 21:25:05 +0530

    Pattern_ButterFly_Pattern

---
+++ Pattern_ButterFly_Pattern.java
@@ -0,0 +1,40 @@
+public class Pattern_ButterFly_Pattern {
+    /**
+     * Print the Pattern 
+     *      *      *
+     *      **    **
+     *      ***  ***
+     *      ********
+     *      ********
+     *      ***  ***
+     *      **    **
+     *      *      *
+     */
+    public static void main(String[] args) {
+        int m=4;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j=m;j>i;j--){
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = m; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j=m;j>i;j--){
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
